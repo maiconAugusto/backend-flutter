373b0101ad1293efd32ec15bd1102281e457c87c
Maicon augusto <maiconaugustocarvalho@gmail.com>
2025-03-11 23:13:40 -0400

    create services/fetch data

---
+++ backend/src/services/index.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const fetchData = async ({ url }) => {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/'${url}`);
+    return response.data;
+};
+
+export default { fetchData };
