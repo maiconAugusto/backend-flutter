8e0e4cd2354c009749466725b50346664ba85fec
Maicon augusto <maiconaugustocarvalho@gmail.com>
2025-03-11 23:05:16 -0400

    add nodemon and routes

--- backend/index.js
+++ backend/index.js
@@ -3,5 +3,5 @@ import app from './src/app.js';
 const PORT = 8000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`server running on port ${PORT}`);
 });
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import cors from 'cors';
+import routes from './routes/index.js'
 
 const app = express();
 app.use(cors());
+app.use(routes);
 
 
 export default app;
